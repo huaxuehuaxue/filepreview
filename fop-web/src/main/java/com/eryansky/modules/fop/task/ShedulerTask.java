feb62c8c28cf3d6b00fdb466a1cdc80c6f1eb755
eryan <eryanwcp@gmail.com>
2018-12-13 17:10:44 +0800

    初始化

--- fop-web/src/main/java/com/eryansky/modules/fop/task/ShedulerTask.java
+++ fop-web/src/main/java/com/eryansky/modules/fop/task/ShedulerTask.java
@@ -1,17 +1,26 @@
 package com.eryansky.modules.fop.task;
 
 import com.eryansky.modules.fop.utils.DeleteFileUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ShedulerTask {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
     @Value("${file.dir}")
     String fileDir;
 
-    //    @Scheduled(cron = "0 0 23 * * ?")   //每晚23点执行一次
+    /**
+     * 每晚23点执行一次
+     */
+    @Scheduled(cron = "0 0 23 * * ?")
     public void clean() {
-        System.out.println("执行一次清空文件夹");
+        log.info("执行一次清空文件夹");
         DeleteFileUtil.deleteDirectory(fileDir);
     }
 }
